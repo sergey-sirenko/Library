a6dbb0c2ffddc972368c0de6916534c2e9ab135e
sergey-sirenko <s7@inbox.ru>
2026-09-10 18:32:41 +0300

    Выпусти версию 1.3.0

--- src/uTypes.pas
+++ src/uTypes.pas
@@ -6,7 +6,7 @@ interface
 
 const
   APP_NAME = 'Библиотека';
-  APP_VERSION = '1.2.0';
+  APP_VERSION = '1.3.0';
   DATA_SIGNATURE = 'LIBRA001';
   INDEX_SIGNATURE = 'LIBRAIDX';
   FORMAT_VERSION = 3;
